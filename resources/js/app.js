db0bec72815c466e63ed58032bb3f3c532c95793
jin1125 <shogo.jinta@gmail.com>
2022-05-25 23:31:04 +0900

    3

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,23 @@
-require('./bootstrap');
+import './bootstrap'
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
+import '../css/app.css'
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => require(`./Pages/${name}.vue`),
+    resolve: async name => {
+        if (import.meta.env.DEV) {
+            return await import(`./Pages/${name}.vue`)
+        } else {
+            let pages = import.meta.glob('./Pages/**/*.vue')
+            const importPage = pages[`./Pages/${name}.vue`]
+            return importPage().then(module)
+        }
+    },
     setup({ el, app, props, plugin }) {
         return createApp({ render: () => h(app, props) })
             .use(plugin)
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,4 @@
-window._ = require('lodash');
+import _ from 'lodash'
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -6,9 +6,8 @@ window._ = require('lodash');
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-window.axios = require('axios');
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+ import axios from 'axios'
+ axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
@@ -16,7 +15,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from 'laravel-echo';
+// import Echo from 'laravel-echo';resources/js/app.js
 
 // window.Pusher = require('pusher-js');
 
