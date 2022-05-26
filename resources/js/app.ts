77a2f7ed9049e8f974df98c074cadb6a3cfaecfe
jin1125 <shogo.jinta@gmail.com>
2022-05-26 23:05:40 +0900

    6

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -19,7 +19,7 @@ createInertiaApp({
         }
     },
     setup({ el, app, props, plugin }) {
-        return createApp({ render: () => h(app, props) })
+        createApp({ render: () => h(app, props) })
             .use(plugin)
             .mixin({ methods: { route } })
             .mount(el);
