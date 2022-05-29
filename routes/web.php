88ba836ba02b2db61d181ff8e5d51ed765a722b9
jin1125 <shogo.jinta@gmail.com>
2022-05-30 08:20:22 +0900

    19

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,12 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-    ]);
+    return Inertia::render('Welcome');
 });
 
 Route::middleware([
     'auth:sanctum',
-    config('jetstream.auth_session'),
+    config('jetstream.pauth_session'),
     'verified',
 ])->group(function () {
     Route::get('/dashboard', function () {
