58355b55bb0c021525c9bb535646651ffa23d3b3
jin1125 <shogo.jinta@gmail.com>
2022-05-29 10:09:05 +0900

    13

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,9 @@ module.exports = {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                blue: '#3B5998',
+            }
         },
     },
 
