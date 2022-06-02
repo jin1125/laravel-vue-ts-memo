8666bf67bd4d812bbde778d4ce4363a41f8a3d46
jin1125 <shogo.jinta@gmail.com>
2022-06-02 10:09:06 +0900

    36

--- app/Http/Requests/CreateMemoRequest.php
+++ app/Http/Requests/CreateMemoRequest.php
@@ -24,7 +24,9 @@ class CreateMemoRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'title'  => 'required',
+            'detail' => 'required',
+            'limit'  => 'required',
         ];
     }
 }
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -21,6 +21,7 @@ const onAddClick = () => {
     <h1 class="text-blue text-2xl font-bold text-center mt-5 mb-10">
       New Memo
     </h1>
+
     <form @submit.prevent="" class="space-y-3">
       <div class="grid grid-cols-3">
         <label for="title" class="text-blue text-xl font-bold col-span-1">
@@ -36,7 +37,11 @@ const onAddClick = () => {
           name="title"
           id="title"
         >
+      <p class="col-start-2 col-span-2 font-bold text-red-500 text-sm">
+        {{ addMemo.errors.title }}
+      </p>
       </div>
+
       <div class="grid grid-cols-3">
         <label for="detail" class="text-blue text-xl font-bold col-span-1">
           detail
@@ -50,19 +55,28 @@ const onAddClick = () => {
           name="detail"
           id="detail"
         ></textarea>
+        <p class="col-start-2 col-span-2 font-bold text-red-500 text-sm">
+          {{ addMemo.errors.detail }}
+        </p>
       </div>
+
       <div class="grid grid-cols-3">
         <label for="limit" class="text-blue text-xl font-bold col-span-1">
           limit
         </label>
         <input
           type="date"
+          name="limit"
           v-model="addMemo.limit"
           class="border border-blue col-span-2 text-blue
             text-sm font-bold py-1 px-2"
           :min="new Date().toISOString().split('T')[0]"
         >
+        <p class="col-start-2 col-span-2 font-bold text-red-500 text-sm">
+          {{ addMemo.errors.limit }}
+        </p>
       </div>
+
       <div class="text-center pt-5 pb-2">
         <button
           @click="onAddClick"
