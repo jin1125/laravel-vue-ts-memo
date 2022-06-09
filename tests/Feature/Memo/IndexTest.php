2bc274b084beb7e515b05353ba6849441002cba0
jin1125 <shogo.jinta@gmail.com>
2022-06-09 22:56:20 +0900

    89

--- tests/Feature/Memo/IndexTest.php
+++ tests/Feature/Memo/IndexTest.php
@@ -2,18 +2,31 @@
 
 namespace Tests\Feature\Memo;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Inertia\Testing\AssertableInertia;
 use Tests\TestCase;
 
 class IndexTest extends TestCase
 {
     use RefreshDatabase;
 
+    private $user;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = User::factory()->create();
+    }
+
     public function test_index_successed()
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $this->actingAs($this->user);
+        $this->get('/memo')
+            ->assertStatus(200)
+            ->assertInertia(fn (AssertableInertia $page) => $page
+            ->component('Index'));
     }
 }
