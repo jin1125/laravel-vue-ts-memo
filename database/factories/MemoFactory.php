3274637f01f273fd15c4a3231268ef5824c06d06
jin1125 <shogo.jinta@gmail.com>
2022-06-10 11:20:36 +0900

    92

--- database/factories/MemoFactory.php
+++ database/factories/MemoFactory.php
@@ -3,13 +3,15 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use App\Models\User;
+use App\Models\Memo;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Memo>
  */
 class MemoFactory extends Factory
 {
+    protected $model = Memo::class;
+
     /**
      * Define the model's default state.
      *
--- tests/Feature/Memo/IndexTest.php
+++ tests/Feature/Memo/IndexTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Memo;
 
+use App\Models\Memo;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Inertia\Testing\AssertableInertia;
@@ -18,12 +19,23 @@ class IndexTest extends TestCase
          */
         /** @var \Illuminate\Contracts\Auth\Authenticatable $user */
         $user = User::factory()->create();
-        $this->user = User::factory()->create();
+        $memos = Memo::factory()->create(['user_id' => 1]);
+
+        // dd($memos->title);
+
         $this->actingAs($user);
-        $this->get('/memo')
+        $this->get(route('memo.index', $memos))
             ->assertStatus(200)
             ->assertInertia(fn (AssertableInertia $page) => $page
-            ->component('Index'));
+            ->component('Index')
+            // ->has('memos', fn (AssertableInertia $page) => $page
+            //     ->where('id', 1)
+            //     ->where('title', $memos->title)
+            //     ->where('status', 'incomplete')
+            //     ->where('detail', $memos->detail)
+            //     ->where('limit', $memos->limit)
+            // )
+        );
     }
 
     public function testNonLoginUserByIfWelcomePageRedirectIs()
